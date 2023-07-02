1b94e5784c4bbbbd071f5318550977e5c9fbde8e
Mark Ghian Francis <zetsuya6167@gmail.com>
2023-07-02 14:06:36 +0800

    Added functionality for input range for creating grid using input range

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
-let gridBoard = document.querySelector('.grid-board');
-//Creating the grid
-gridBoard.style.gridTemplateColumns = "repeat(16, 1fr)";
-gridBoard.style.gridTemplateRows= "repeat(16, 1fr)";
+let valueOutput = document.querySelector("#range-value");
+let sliderValue = document.querySelector("#range-input");
 
-for ( let x = 0; x < 256; ++x ) {
-  let createDiv = document.createElement('div');
-  createDiv.style.backgroundColor = '#fefefe';
-  gridBoard.insertAdjacentElement('beforeend', createDiv);
-}
+//Default value
+valueOutput.textContent = `${sliderValue.value} x ${sliderValue.value}`;
+
+//Getting the current value
+sliderValue.addEventListener("input", (e) => {
+  valueOutput.textContent = `${e.target.value} x ${e.target.value}`;
+  creatingGrid(e.target.value);
+});
+
+//This function create the grids
+const creatingGrid = (value) => {
+  let gridBoard = document.querySelector(".grid-board");
+  //Creating the grid
+  gridBoard.style.gridTemplateColumns = `repeat(${value}, 1fr)`;
+  gridBoard.style.gridTemplateRows = `repeat(${value}, 1fr)`;
+
+  let size = value * value;
+
+  for (let x = 0; x < size; ++x) {
+    let createDiv = document.createElement("div");
+    createDiv.style.backgroundColor = "#fefefe";
+    gridBoard.insertAdjacentElement("beforeend", createDiv);
+  }
+};
+
+//Default number of grids
+creatingGrid(16);
